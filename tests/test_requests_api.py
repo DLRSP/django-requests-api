90966dfe241b86159ddb869e4abe5a573a7317c7
dlrsp-dev <dlrsp.dev@gmail.com>
2023-06-27 09:17:44 +0200

    - add initialization tests

--- tests/test_requests_api.py
+++ tests/test_requests_api.py
@@ -2,6 +2,7 @@
 import logging
 
 from django.test import TestCase
+from requests_api import RequestsApi
 
 LOGGER = logging.getLogger(name="django-requests-api")
 
@@ -17,4 +18,14 @@ class ErrorsTestCase(TestCase):
         """Remove Test Data"""
         LOGGER.debug("Tests tearDown")
 
+    def test_init_client(self):
+        """Test the initialization of client"""
+        LOGGER.debug("Test the initialization of client")
+        api_client = RequestsApi("https://fakeapiurl.com")
+        self.assertIsInstance(api_client, RequestsApi)
+
+    def test_init_client_without_parameters(self):
+        """Test the initialization of client without parameters"""
+        LOGGER.debug("Test the initialization of client without parameters")
+        self.assertRaises(TypeError, lambda: RequestsApi())
 
