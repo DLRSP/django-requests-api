274ca229100a41c5a83f17b749d822f4bb050e42
dlrsp-dev <dlrsp.dev@gmail.com>
2023-07-30 23:11:40 +0200

    Bump version: 0.3.2 → 0.3.3-dev

--- src/requests_api/__init__.py
+++ src/requests_api/__init__.py
@@ -1,7 +1,8 @@
-import django
-from .requests_api import RequestsApi
-
-__version__ = "0.3.2"
+"""
+See PEP 386 (https://peps.python.org/pep-0386/)
+"""
+__version__ = "0.3.3-dev"
+__version_info__ = tuple(int(i) if i.isdigit() else i for i in __version__.split("."))
 __license__ = "MIT"
 __title__ = "requests_api"
 
@@ -9,14 +10,10 @@ __author__ = "DLRSP"
 __copyright__ = "Copyright 2010-present DLRSP"
 
 # Version synonym
-VERSION = __version__
+VERSION = __version_info__
 
 # Header encoding (see RFC5987)
 HTTP_HEADER_ENCODING = "iso-8859-1"
 
 # Default datetime input and output formats
 ISO_8601 = "iso-8601"
-
-if django.VERSION < (3, 2):
-    default_app_config = "requests_api.apps.RequestsApiConfig"
-
